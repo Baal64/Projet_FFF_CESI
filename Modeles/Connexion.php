3bd2cc6e3c8cad4dd2afaf9fc523ffe5ae078b6b
Barbara Levert <barbara.levert@viacesi.fr>
2021-01-19 15:52:18 +0100

    Update 16h45 (Vues Presentateur)

--- Modeles/Connexion.php
+++ Modeles/Connexion.php
@@ -1,14 +1,28 @@
 <?php
+//session_start();
 
+class Connexion extends Manager{
 
-abstract class Connexion
-{
+    private $utilisateur;
 
-    private $db;
-
-    public function __construct(){
-        $this->db = new PDO('mysql:host=localhost;dbname=fff;charset=utf8','root','',[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    public function connect($identifiant,$mdp){
+        if(isset($_SESSION['connected']) && $_SESSION['connected']==true) {
+            $this->utilisateur = $_SESSION['data_connected_user'];
+        }
+        else {
+            $s = "SELECT * FROM utilisateurs WHERE (BINARY id_connexion='$identifiant' OR email_utilisateur='$identifiant') AND BINARY mdp_connexion='$mdp'";
+            $r = $this->db->query($s);
+            if($utilisateurData = $r->fetch(PDO::FETCH_ASSOC)) {
+                $this->utilisateur = new Utilisateur($utilisateurData);
+                $_SESSION['connected'] = true;
+                $_SESSION['data_connected_user'] = $utilisateurData;
+            }
+            return true;
+        }
     }
 
+    public function getUser(){
+        return $this->utilisateur;
+    }
 
 }
