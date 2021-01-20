8ff9d006cc74b6b89fe07fcf26ae06837b31e1bc
Alexandre Ba <alexandre64.ba@gmail.com>
2021-01-20 15:29:45 +0100

    Merge branch 'NewMVC' into Alex3
    # Conflicts:
    #	index.php

---
+++ controleurs/edition_equipe_match.php
@@ -0,0 +1,21 @@
+<?php
+if(session_status()==PHP_SESSION_NONE)
+    session_start();
+
+if(file_exists('../php/fonctions.php'))
+    require_once('../php/fonctions.php');
+// Fonction de chargement de classe
+spl_autoload_register('chargerClass');
+
+
+$joueurManager = new joueurManager();
+$joueurCollection = $joueurManager->readAll();
+
+$listePostes = array('Avant', 'Ailier', 'Milieu offensif', 'Milieu', 'Milieu défensif', 'Arrière', 'Gardien');
+
+$listePlacements = array('Centre', 'Droit', 'Gauche');
+
+$utilisateur = new UtilisateurManager();
+
+
+$view = "vue_creation_equipe_match";
